2918c9345a8b62430370bac5e7878e69601fb86e
Ryan Norris <thenameisnigel@users.noreply.github.com>
2014-07-28 15:28:12 -0400

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,12 +5,18 @@ USE_CAMERA_STUB := true
 
 TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi
 TARGET_BOOTLOADER_BOARD_NAME := vs840
 TARGET_ARCH := arm
 BOARD_KERNEL_CMDLINE := vmalloc=450M, console=ttyDCC0,115200,n8 androidboot.hardware=qcom
 BOARD_KERNEL_BASE := 0x40200000
 BOARD_KERNEL_PAGESIZE := 2048
+
+# Architecture
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_ABI2 := armeabi
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
 
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
